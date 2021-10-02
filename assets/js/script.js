b7e074815f8d015ab57eaa8a5d0fa111b4fb0c24
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-01 20:00:27 -0400

    added script to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var searchHistory=[];
 var APIkey = "ea3cc374537ac734a5e9057bfff093d4";
 var rootURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
 
-var serachForm = document.querySelector("#search-form");
+var searchForm = document.querySelector("#search-form");
 var searchInput = document.querySelector("#search-input");
 var currentWeather = document.querySelector("#today");
 var forecastWeather = document.querySelector("#forecast");
@@ -11,12 +11,22 @@ var searchHistoryContainer = document.querySelector("#history");
 function getLatLong(event) {
     event.preventDefault();
     console.log(event);
+    var city=searchInput.value.trim();
+    var coordinatesURL = rootURL + "/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIkey;
+    console.log(city);
+
+
+    fetch(coordinatesURL)
+    .then(function (response) {
+        return response.json()
+    }). then(function(data){
+        console.log(data);
+    })
 }
 
 
 
 
-
-serachForm.addEventListener("submit", )
+searchForm.addEventListener("submit", getLatLong)
 
 
