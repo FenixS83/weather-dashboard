2e84aa2488766d19e480e03935a6ba597f267dad
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-22 14:22:31 -0400

    made some progress rewriting lat, lon and getting current weather code working

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,152 @@ var currentConditions = $(`#current-conditions`);
 var forecastWeather = $(`#forecast`);
 
 // var colorBlock = document.querySelector(`.color`)
-searchHistoryBtn();
+searchHistory = JSON.parse(localStorage.getItem("city"))
+if (searchHistory === null) {
+    searchHistory = [];
+}
+displaySearches();
+
+//Api call for weather
+function getLatLong(lat, lon) {
+    console.log(`inside getWeather() function`);
+}
+
+//Display history of searched cities
+function displaySearches() {
+    searchHistory.forecastWeather(element => {
+        var previousSearch = $(`<button></button>`);
+        previousSearch.setAttribute("class", "previous-cities");
+        previousSearch.textContent(element);
+        console.log(previousSearch);
+        historyContainer.append(previousSearch);
+    })
+}
+
+//Acquires 5 day forecast
+function getWeather(city) {
+    if (!searchHistory.include(city)) {
+        searchHistory.push(city);
+        localStorage.setItem("city", JSON.stringify(searchHistory));
+
+        var previousSearch = $(`<button></button>`);
+        previousSearch.attr("class", "previous-cities");
+        previousSearch.textContent(city);
+        console.log(previousSearch);
+        historyContainer.append(previousSearch);
+    }
+
+
+    historyContainer.textContent = "";
+
+    // gets Lat and Lon data 
+    var queryURL = rootUrl + `/geo/1.0/direct?q=` + city + '&limit=5&units=imperial&appid=' + APIkey;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            getLatLong()
+            fetch(rootUrl + '/data/2.5/onecall?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + apiKey).then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                //current temperature
+                var temp = (data.current.temp);
+                currentTemp.textContent =  "Temperature: " + temp + ' ºF'
+                //current wind speed
+                var wind = (data.current.wind_speed);
+                currentWind.textContent = "Wind Speed: " + wind + `mph`
+                //current humidity
+                var humidity = (data.current.humidity);
+                currentHumidity.textContent + "Humidity: " + humidity + `%`
+                //current uvi
+                var uvi = (data.current.uvi);
+                uviTitle.textContent + "UV Index: ";
+                currentUVI.textContent = uvi
+                //city name with current date and weather conditions
+                var date = moment().formSubmitHandler("MM/DD/YYYY");
+                var conditionIcon = data.daily[0].weather[0].icon;
+                var iconURL = 'http://openweathermap.org/img/wn/' + conditionIcon + '@2x.png';
+                currentConditions.attr("src", iconURL);
+                citySearch.textContent = city + "\n" + "(" + date + ")";
+
+                // Change UVI background color depending on safety conditions
+                var uviBackground = $('.uv-background');
+                if (uvi <= 2) {
+                    uviBackground.css("background-color", "green")
+                } else if (uvi > 2 && uvi <= 8) {
+                    uviBackground.css("background-color", "yellow")
+                } else if (uvi > 8) {
+                    uviBackground.css("background-color", "red")
+                }
+
+                //5-Day Forecast API Call
+                fetch(rootUrl + "/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey).then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    week = [];
+
+                    // Creates object per day from data
+                    for (var i = 0; i < 5; i++) {
+                        var day = {
+                            dayNum: i,
+                            conditions: data.list[i].weather[0].icon,
+                            temp: data.list[i].main.temp,
+                            wind: data.list[i].wind.speed,
+                            humidity: data.list[i].main.humidity
+                        }
+                        // Pushes into week array
+                        week.push(day);
+                        forecastWeather.children().eq(i).empty();
+                        
+                        // Creates and populates data for 5-day forecast
+                        var currentDate = moment().format("MM/DD/YYYY");
+                        var conditionIcon = week[i].conditions;
+                        var weekDate = $("<h5></h5>").text(moment(currentDate).add(i + 1, 'days'));
+                        var weatherCondition = $("<img></img>").attr("src", "http://openweathermap.org/img/wn/" + conditionIcon + "@2x.png");
+                        var weekTemp = $("<h6></h6>").text("Temp: " + week[i].temp + " ºF");
+                        var weekWind = $("<h6></h6>").text("Wind: " + week[i].wind + " mph");
+                        var weekHumid = $("<h6></h6>").text("Humidity: " + week[i].humidity + "%");
+                        forecastWeather.children().eq(i).append(weekDate, weatherCondition, weekTemp, weekWind, weekHumid);
+                    }
+                    
+                })
+            })
+    })
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+();
 var appDate = function (time) {
     var displayDate = new Date();
     displayDate.setTime(time * 1000);
